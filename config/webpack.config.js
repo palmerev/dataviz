a04a27e09cf4c7d2b8ba90455812520dd6bd6c47
Evan Palmer <evjpalmer@gmail.com>
2016-07-29 14:09:17 -0700

    add devTool: source-map to webpack config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -24,5 +24,6 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
-  ]
+  ],
+  devTool: 'source-map'
 };
